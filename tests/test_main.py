180b784a940483de8984c84fa08a340228453ee5
Jose pablo Navarro <josepablo@MacBook-Pro-de-Jose.local>
2026-08-20 15:44:30 -0400

    Fix test formatting

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,11 @@
-from src.main import sumar ,restar, multiplicar
+from src.main import sumar, restar, multiplicar
 
 def test_sumar():
-    assert sumar(2,3)==5
+    assert sumar(2, 3) == 5
 
 def test_restar():
-    assert restar(10,4)==6
+    assert restar(10, 4) == 6
 
 def test_multiplicar():
-    assert multiplicar(3,4)==12
+    assert multiplicar(3, 4) == 12
+    
