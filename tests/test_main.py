9ccaf19fa799e60854166920bb23f3f9c8fcba93
Jose pablo Navarro <josepablo@MacBook-Pro-de-Jose.local>
2026-08-20 15:22:41 -0400

    Add pytest tests

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+from src.main import sumar ,restar, multiplicar
+
+def test_sumar():
+    assert sumar(2,3)==5
+
+def test_restar():
+    assert restar(10,4)==6
+
+def test_multiplicar():
+    assert multiplicar(3,4)==12
